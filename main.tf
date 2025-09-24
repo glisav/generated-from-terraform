8d2be53bed3ec3686eb3c55916276b2779df8802
Glisav Katroshi <glisav.katroshi@gmail.com>
2025-09-24 19:13:30 +0200

    Remove sensitive data from main.tf
    Removed sensitive AWS credentials and AMI ID.

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
   region     = "eu-central-1"
-  access_key = "AKIA2ZHWCUOKINUOAG7Q"
-  secret_key = "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"
+  access_key = " "
+  secret_key = " "
 }
 
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-02ff57195b457bd9a"
+  ami           = " "
   instance_type = "t2.micro"
   tags = {
     Name = "MyFirstEC2"
